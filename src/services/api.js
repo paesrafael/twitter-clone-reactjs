4e5c93d6a506b710d96128a656c5120f07752180
Rafael Paes <rfaelpaes@gmail.com>
2020-09-15 22:19:45 -0300

    adiciona dados do usuário

---
+++ src/services/api.js
@@ -0,0 +1,5 @@
+const userProfileName = 'Rafael Paes'
+const userProfileNick = '@paesrfael'
+const userProfileWork = 'Desenvolvedor frontend na POINTEC'
+
+export { userProfileName, userProfileNick, userProfileWork }
